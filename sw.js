e2e1a0a4d920d705f1aee7bef7ed4f6e002efc2c
jaysolution07 <jaysolution07@gmail.com>
2026-04-24 18:23:59 +0200

    Refactor service worker fetch event handling

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => clients.claim());
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => new Response(''))));
+
+self.addEventListener('activate', e => self.clients.claim());
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
